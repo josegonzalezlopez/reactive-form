8aae9b5a7985429fc4a42929831e94e1d6fdd726
Jose Maria Gonzalez Lopez <josegonzalezlopez@gmail.com>
2023-07-18 21:57:37 -0300

    dynamic form wip

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const initialProduct = {
+  name: 'Cafetera express con molinillo',
+  price: 23500,
+  inStorage: 9,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset( initialProduct );
+  }
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]],
+  });
+
+  onSave(): void{
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    } 
+    console.log(this.myForm.value);
+    this.myForm.reset( { price: 0, inStorage: 0 });
+  }
 }
